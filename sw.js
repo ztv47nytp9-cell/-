e8874c6956ec778c88fb7c0630576f741bab2d1f
ztv47nytp9-cell <ztv47nytp9@privaterelay.appleid.com>
2026-07-21 21:39:18 +0900

    Use full screen menu panel

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "victor-alpha-0-19-0m-cache-v70";
+const CACHE_NAME = "victor-alpha-0-19-0m-cache-v71";
 const CORE_ASSETS = [
   "./index.html",
-  "./style.css?v=0190m70",
-  "./storage.js?v=0190m70",
-  "./app.js?v=0190m70",
+  "./style.css?v=0190m71",
+  "./storage.js?v=0190m71",
+  "./app.js?v=0190m71",
   "./manifest.json",
   "./icons/icon-180.png",
   "./icons/icon-192.png",
